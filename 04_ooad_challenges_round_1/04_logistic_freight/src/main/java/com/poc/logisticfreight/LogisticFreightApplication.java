5b401ff2a6860d8514c29837437d1934377e28b7
Diego Umpierre <diego@umpierre.com.br>
2026-05-27 09:12:00 -0300

    scaffold do logistic freight a partir do template

---
+++ 04_ooad_challenges_round_1/04_logistic_freight/src/main/java/com/poc/logisticfreight/LogisticFreightApplication.java
@@ -0,0 +1,12 @@
+package com.poc.logisticfreight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticFreightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticFreightApplication.class, args);
+    }
+}
